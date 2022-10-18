b97f02cff0decadc92345258cec1739cd006ebba
unknown <valen.martu2206@gmail.com>
2022-10-17 22:12:39 -0300

    Creando todas las funciones del controlador de los productos

--- src/controllers/product_controller.ts
+++ src/controllers/product_controller.ts
@@ -15,7 +15,7 @@ class ProductController {
     }
   }
 
-  public async getProductById(req: Request, res: Response) {
+  public async getProductById(req: Request, res: Response): Promise<void> {
     // Devuelve un producto especifico
     const { id } = req.params;
     try {
@@ -29,7 +29,96 @@ class ProductController {
   }
   // post ---------------------------------------
 
+  public async createProduct(req: Request, res: Response): Promise<void> {
+    // Crear un nuevo producto
+    const { name, price, stock, sizes, description, tags } = req.body;
+    try {
+      const product = await prisma.product.create({
+        data: {
+          name,
+          price,
+          stock,
+          sizes,
+          description,
+          tags,
+        },
+      });
+      res.json(product);
+    } catch (e) {
+      res.send("Error al crear el producto: " + e);
+    }
+  }
+
   // put ---------------------------------------
 
+  public async updateStock(req: Request, res: Response): Promise<void> {
+    // recibe stockChange como valor numerico positivo o negativo, esa es la variación en el stock anterior
+    const { id, stockChange } = req.body;
+    try {
+      if (stockChange > 0) {
+        const result = await prisma.product.update({
+          where: { id },
+          data: {
+            stock: {
+              increment: stockChange,
+            },
+          },
+        });
+        res.json(result);
+      } else {
+        const result = await prisma.product.update({
+          where: { id },
+          data: {
+            stock: {
+              increment: stockChange,
+            },
+          },
+        });
+        res.json(result);
+      }
+    } catch (e) {
+      res.send(
+        "Error al cambiar el stock del producto de ID: " + id + "Error: " + e
+      );
+    }
+  }
+
+  public async updateProductDetails(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    //actualiza los datos de un producto
+    const { id, changes } = req.body;
+    try {
+      const result = await prisma.product.update({
+        where: { id },
+        data: {
+          ...changes,
+        },
+      });
+      res.json(result);
+    } catch (e) {
+      res.send(
+        "Error al cambiar datos del producto de ID: " + id + "Error: " + e
+      );
+    }
+  }
+
   // delete ---------------------------------------
+
+  public async deleteProduct(req: Request, res: Response): Promise<void> {
+    const { id } = req.body;
+    try {
+      const result = await prisma.product.delete({
+        where: { id },
+      });
+      res.json(result);
+    } catch (e) {
+      res.send("Ha ocurrido un error al eliminar el producto: " + e);
+    }
+
+    // IMPORTANTE: debe autenticar al ususario y chequear que sea admin
+  }
 }
+
+export const productController = new ProductController();
